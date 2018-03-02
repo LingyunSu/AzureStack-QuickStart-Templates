5dd2b7838008892ed2400dda788e66f6bfadb016
Ubuntu <azureuser@vmj7ko6tfbqcvis.bebad232-8f0b-425b-9b75-e58659e5f1e0.internal.azurestack.local>
2018-03-02 00:52:07 +0000

    second check in for minikube test

--- 101-vm-linux-minikube/PostDeploymentValidation/helloworld_on_minikube_test.sh
+++ 101-vm-linux-minikube/PostDeploymentValidation/helloworld_on_minikube_test.sh
@@ -1,5 +1,4 @@
-#! /bin/sh
-
+#! /bin/bash
 #Define Console Output Color
 RED='\033[0;31m'
 GREEN='\033[0;32m'
@@ -19,24 +18,72 @@ if [ -z $isMinikubeRunning ]; then
 
   # Check status again
   isMinikubeRunning="$(sudo minikube status | grep 'minikube: Running')"
-  if [ -z $isMinikubeRunning ]; then
-    echo "${RED}Failed to start minikube. Please troubleshoot the root cause.${NC} "
+  if [[ -z $isMinikubeRunning ]]; then
+    echo -e "${RED}Failed to start minikube. Please troubleshoot the root cause.${NC} "
     exit 3
   else
-    echo "${GREEN}Minikube is started.${NC}"
+    echo -e "${GREEN}Minikube is started.${NC}"
   fi
 else
-  echo "${GREEN}Minikube is started.${NC}"
+  echo -e "${GREEN}Minikube is started.${NC}"
 fi
 
-# Now start install helloworld application on mikikube
-echo "Running hello-world on minikube"
+# Run helloworld application on mikikube
+echo "Run hello-world on minikube..."
 sudo kubectl run helloworld --image=msazurestackdocker/linsuhyperkube:v1 --port=8080
+
+# Check pod status
+i=0
+isPodRunning=0
+while [ $i -lt 10 ];do
+  podstatus="$(sudo kubectl get pods | grep 'Running')"
+
+  if [[ -z $podstatus ]]; then
+    echo "Tracking helloworld pod status..."
+    sleep 10s
+  else
+    echo -e "${GREEN}Pod is running.${NC}"
+    isPodRunning=1
+    break
+  fi
+  let i=i+1
+done
+
+# Test fail if the pod is not running
+if [ $isPodRunning -eq 0 ]; then
+  echo -e "${RED}Validation failed because the pod for hello-world app is not running.${NC}"
+  exit 3
+fi
+
+# Expose hello-world app
+echo "Expose hello-world app..."
 sudo kubectl expose deployment helloworld --type=LoadBalancer
 
+i=0
+while [ $i -lt 10 ];do
+  # Retrive external IP
+  externalIp=$(sudo kubectl get services helloworld -o=custom-columns=NAME:.status.loadBalancer.ingress[0].ip | grep -oP '(\d{1,3}\.){1,3}\d{1,3}')
 
-echo "${GREEN}Minikube post-deployment validation pass!${NC}"
-exit 0
+  if [[ -z $externalIp ]]; then
+    echo "Tracking helloworld servic status..."
+    sleep 10s
+  else
+    echo "External IP for helloworld:"$externalIps
+  fi
+  let i=i+1
+done
 
+if [[ -z $externalIp ]]; then
+  echo -e "${RED}Validation failed because the external ip for hello-world app is not available.${NC}"
+  exit 3
+fi
 
-
+appurl="http://"$externalIp":8080"
+appContent="$(curl ${appurl})"
+if [ $appContent -eq "Hello World!" ]; then
+  echo -e "${GREEN}Minikube post-deployment validation pass!${NC}"
+  exit 0
+else
+  echo -e "${RED}Validation failed because the app is not return right content. App return:"$appContent "${NC}"
+  exit 3
+fi
